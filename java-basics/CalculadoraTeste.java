81934871bcdc4ce8d67ff6095f13d62d1499bab9
manozita <manoelamartedi@hotmail.com>
2024-03-06 20:16:15 -0300

    calculadora sem try
    mesmo funcionamento do programa anterior, entretanto sem o try command, usando, ao invés dele, if.

--- java-basics/CalculadoraTeste.java
+++ java-basics/CalculadoraTeste.java
@@ -6,33 +6,51 @@ public class CalculadoraTeste {
 			return;
 		}
 		
-		try {
-			int n1 = Integer.parseInt(args[0]);
-			int n2 = Integer.parseInt(args[2]);
-			
-			char operacao = args[1].charAt(0);
-		
-			switch (operacao) {
-				case '+':
-					System.out.println(n1 + " + " + n2 + " = " + (n1+n2));
-					break;
-				case '-':
-					System.out.println(n1 + " - " + n2 + " = " + (n1-n2));
-					break;
-				case 'x':
-				case '*':
-					System.out.println(n1 + " x " + n2 + " = " + (n1*n2));
-					break;
-				case '/':
-					System.out.println(n1 + " / " + n2 + " = " + (n1/n2));
-					break;
-				default:
-					System.out.println("Operacao invalida. Tente +, -, x ou /");
-			}
-		} catch (NumberFormatException e) {
+		if (!isNumber(args[0]) || (!isNumber(args[2]))) {
 			System.out.println("Insira no formato 'numero operacao numero'");
+			return;
+		}
+		
+		float n1 = Float.parseFloat(args[0]);
+		float n2 = Float.parseFloat(args[2]);
+			
+		char operacao = args[1].charAt(0);
+		
+		switch (operacao) {
+			case '+':
+				System.out.println(n1 + " + " + n2 + " = " + (n1+n2));
+				break;
+			case '-':
+				System.out.println(n1 + " - " + n2 + " = " + (n1-n2));
+				break;
+			case 'x':
+			case '*':
+				System.out.println(n1 + " x " + n2 + " = " + (n1*n2));
+				break;
+			case '/':
+				System.out.println(n1 + " / " + n2 + " = " + (n1/n2));
+				break;
+			default:
+				System.out.println("Operacao invalida. Tente +, -, x ou /");
 		}
 		
 	}
-
+	
+	public static boolean isNumber(String str) {
+		//checar se contem uma virgula/ponto + checar se os caracteres sao numerais
+		boolean check = false;
+		char c;
+		for (int i = 0; i < str.length(); i++) {
+			c = str.charAt(i);
+			if (c == '.' || c == ',') {
+				if (check)
+					return false;
+				else
+					check = true;
+			}
+			else if (c < '0' || c > '9')
+				return false;
+		}
+		return true;
+	}
 }
